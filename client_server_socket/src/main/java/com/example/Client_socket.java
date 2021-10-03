0d6007f090ea1fef918f67f9625e21fbbb4c2e95
Miki462510 <78083568+Miki462510@users.noreply.github.com>
2021-10-03 21:19:19 +0200

    Update Client_socket.java

--- client_server_socket/src/main/java/com/example/Client_socket.java
+++ client_server_socket/src/main/java/com/example/Client_socket.java
@@ -15,8 +15,25 @@ public class Client_socket {
 
     
     public void comunica(){
-
-        
+        try
+        {
+            System.out.println("4 ... inserisci la stringa da trasmettere al server"+'\n');
+            stringaUtente = tastiera.redLine();
+            //spedisco al server
+            System.out.println("5 ... invio la stringa al server e attendo ... );
+            outVersoServer.writeBytes ( stringaUtente+'\n');
+            //leggo la risposta del server 
+            stringaRicevutaDalServer=inDalServer.redLine();
+            System.out.println("8 ... risposta del server"+ '\n' +stringaRicevutaDalServer);
+            //chiudo connessione
+            System.out.println("9 CLIENT: termina elaborazione e chiude connesione");
+            mioSocket.close();
+        } catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante la comunicazione col server!");
+            System.exit(l);
+        }
     }
     
     
