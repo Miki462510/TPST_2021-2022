95784e24aae35815abccef54760e306eb9f3fabe
Miki462510 <78083568+Miki462510@users.noreply.github.com>
2021-10-06 13:44:41 +0200

    Add files via upload

---
+++ client_server_socket/src/main/java/com/example/App.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class App 
+{
+    public static void main( String[] args ) 
+    {
+        Server_thread servente = new Server_thread();
+        servente.comunica();
+    }
+}
---
+++ client_server_socket/src/main/java/com/example/MultiServer.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class MultiServer {
+    public void start(){
+try{
+
+}
+catch (Exception e){
+
+}
+}
+}
